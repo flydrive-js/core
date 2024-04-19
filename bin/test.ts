7bf8449ffa0972728bc1c63087df64a1cdd76227
Harminder Virk <virk.officials@gmail.com>
2024-04-19 13:39:17 +0530

    refactor: remove files glob since using suites

--- bin/test.ts
+++ bin/test.ts
@@ -5,7 +5,6 @@ import { configure, processCLIArgs, run } from '@japa/runner'
 processCLIArgs(process.argv.splice(2))
 
 configure({
-  files: ['tests/**/*.spec.ts'],
   suites: [
     {
       name: 'main',
